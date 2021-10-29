53e19d74dde893e94cc7dc07451758e3c8572275
avelamer <45433566+avelamer@users.noreply.github.com>
2021-10-29 09:08:43 +0200

    Initial commit

---
+++ src/BattleshipAlexSamuel.java
@@ -0,0 +1,47 @@
+// AUTHORS
+
+// ALEX VELA MERRIEN
+
+// SAMUEL PÉREZ SANDONATO
+
+import java.util.Scanner;
+
+public class BattleshipAlexSamuel {
+
+		public static void main (String [] Args) {
+			
+			// TREMENDO JUEGAZO DE ALEX Y SAMUEL:
+			
+			
+			
+		}
+		
+		public static void start () {
+			
+			// ESTA PARTE SE ENCARGA DE MOSTRAR EN PANTALLA EL INICIO DEL JUEGO
+			// Y  DE PREGUNTAR AL JUGADOR EN QUE MODO DE JUEGO QUIERE JUGAR
+			
+			
+		}
+		public static void shipGeneration () {
+			
+			// ESTA PARTE SE ENCARGA DE GENERAR LOS BARCOS ALEATORIAMENTE
+			// HAY 2 BARCOS DE 2 Y DE 3, HORIZONTALES Y VERTICALES
+			
+			
+		}
+		public static void updateMap () {
+			
+			// ESTA PARTE SE ENCARGA DE ACTUALIZAR EL MAPA DE JUEGO
+			
+			
+			
+			
+		}
+		public static void shootAt (int x, int y) {
+			
+			
+			
+			
+		}
+}
